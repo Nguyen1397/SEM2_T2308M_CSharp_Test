abc010927f6efac7dcff0068902cc55e5121db90
Nguyen1397 <knguyen4582@gmail.com>
2024-06-14 10:25:46 +0700

    Question2

---
+++ KhoiNguyen/Question2/Question2/Guitar_6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    internal class Guitar_6 : Instrumental
+    {
+        public override void GetInfo()
+        {
+            Console.WriteLine("_Guitar_");
+            Console.WriteLine("This type of guitar has 6 strings\n");
+        }
+    }
+}
