a335f1d5a9fe0e6908b8c88974fdb44cc1f1d3ee
Nguyen1397 <knguyen4582@gmail.com>
2024-06-14 09:59:40 +0700

    Question1

---
+++ Khoi_Nguyen/Question1/Question1/NormalAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    internal class NormalAccount : IAccount
+    {
+        private double balance;
+
+        public double Balance { get => balance; set => balance = value; }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"Your balancer: {Balance} đ");
+        }
+
+        public bool IsEnough(double amount)
+        {
+            if(Balance <= amount)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Transfer(double amount)
+        {
+            bool canTransfer = IsEnough(amount);
+            if(canTransfer)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Your transferred: {amount} đ, your balancer: {Balance - amount} đ");
+            }
+            else
+            {
+                Console.WriteLine("Not enough money to transfer");
+            }
+            
+        }
+    }
+}
---
+++ Khoi_Nguyen/Question1/Question1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Question1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("__A Normal Account__");
+        NormalAccount normalAccount = new NormalAccount();
+        normalAccount.Balance = 24000000;
+        normalAccount.CheckBalance();
+        Console.WriteLine("Enter money to transfer: ");
+        int amount1 = Convert.ToInt32(Console.ReadLine());
+        normalAccount.Transfer(amount1);
+
+        Console.WriteLine("__An Exchanged Account__");
+        ExchangedAccount exchangedAccount = new ExchangedAccount();
+        exchangedAccount.Balance = 24000000;
+        exchangedAccount.CheckBalance();
+        Console.WriteLine("Enter money to transfer: ");
+        int amount2 = Convert.ToInt32(Console.ReadLine());
+        exchangedAccount.Transfer(amount2);
+    }
+}
